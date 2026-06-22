bb4529c4d34f513c0ca6dd2652b96ba10aa3119e
Rashmi-kumari123 <rashu2377@gmail.com>
2026-06-22 17:19:40 +0530

    Remove Railway deployment config; use AWS EKS only

--- ai-insights-service/src/main/resources/AiInsightsServiceApplication.java
+++
@@ -1,9 +0,0 @@
-package nexusHR;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class AiInsightsServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(AiInsightsServiceApplication.class, args);
-    }
-}
